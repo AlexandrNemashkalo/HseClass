1f2e83ade036b46910db179740db66aedc5e5277
AlexandrNemashkalo <n.a.vi_talevich@mail.ru>
2021-05-24 13:38:03 +0300

    вернул все назад

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -23,7 +23,7 @@ const vuexLocalStorage = new VuexPersist({
 export default new Vuex.Store({
   state: {
     isDark:false,
-    port: "https://84.201.174.207/api/",  // https://localhost:5001/api/ https://hse-class.ru/api/
+    port: "https://hse-class.ru/api/",  // https://localhost:5001/api/ https://hse-class.ru/api/
     token:null,
     windowWidth:null,
     user:{
